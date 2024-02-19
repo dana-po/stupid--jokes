d9cbc4c297491907ef876594ede06ce7849282ff
dana-po <133027735+dana-po@users.noreply.github.com>
2024-02-19 19:53:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const jokeEl = document.querySelector('#js-joke');
+const btn = document.querySelector('#js-btn');
+
+// generateJoke()// if refresh => a new joke
+
+
+//USING .THEN()
+// function generateJoke(){
+//     fetch('https://icanhazdadjoke.com', {
+//      headers: {
+//         'Accept': 'application/json'
+//      }
+//     }).then(res => res.json()).then(data=>{
+//         jokeEl.innerHTML = data.joke
+//     })
+
+// }
+
+btn.addEventListener('click', generateJoke);
+//USING ASYNC/AWAIT
+async function generateJoke() {
+  const response = await fetch('https://icanhazdadjoke.com', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+
+  const data = await response.json();
+  jokeEl.innerHTML = data.joke;
+}
